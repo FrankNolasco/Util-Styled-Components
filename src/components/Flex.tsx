7a263ea5634fac6139c7ece575e57377a2ac26b2
FranksitoKawai <fnv1298@gmail.com>
2021-07-12 08:55:20 -0500

    .

--- src/components/Flex.tsx
+++ src/components/Flex.tsx
@@ -4,24 +4,30 @@ import { Div } from "./Overrides";
 
 export const Flex = styled(Div)`
   display: flex;
-  justify-content: ${(props : IFlex ) => props.justifyContent ? props.justifyContent : "start"};
-  gap: ${(props) => (props.gap ? props.gap : "0" )};
+  justify-content: ${(props: IFlex) =>
+    props.justifyContent ? props.justifyContent : "start"};
+  gap: ${(props) => (props.gap ? props.gap : "0")};
   * {
-    flex-grow : ${(props)=> props.grow ? 1 : 0}
+    flex-grow: ${(props) => (props.grow ? 1 : 0)};
   }
   @media (max-width: 768px) {
-    flex-direction: ${(props) => props.responsiveReorder ? "column" : "row"};
+    flex-direction: ${(props) => (props.responsiveReorder ? "column" : "row")};
   }
 `;
 export const Column = styled(Flex)`
   flex-direction: column;
+  @media (max-width: 768px) {
+    flex-direction: ${(props) =>
+      props.responsiveReorder ? "row" : "column"};
+  }
 `;
 
 export const Row = styled(Flex)`
   flex-direction: row;
   align-items: center;
   @media (max-width: 768px) {
-    flex-direction: ${(props) => props.responsiveReorder ? "column" : "row"};
+    flex-direction: ${(props) =>
+      props.responsiveReorder ? "column" : "row"};
   }
 `;
 
